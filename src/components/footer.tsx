ca315799398ab16c72ef4c8c9c037f8e05b194c4
Michael Gitonga <gitoshmbae@gmail.com>
2025-04-10 13:52:09 +0300

    Adjust the UIs of  src/components/homePage/QuoteSection.tsx & src/components/footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <>
       <footer className="conatiner px-4 py-20">
-        <div className="flex justify-between items-center bg-green-800 rounded-xl px-8 py-10">
+        <div className="flex justify-between items-center bg-green-800 text-slate-100 rounded-xl px-8 py-10">
           <div className="w-4/12 space-y-4">
             <p className="text-5xl leading-12">
               Your Trusted Partner in Logistics Solutions
@@ -14,18 +14,18 @@ export default function Footer() {
           </div>
           <div className="flex justify-between items-start gap-x-8">
             <div className="c">
-              <ul className="list-none space-y-4">
-                <li className="c">About us</li>
-                <li className="c">Our Services</li>
-                <li className="c">Testimonials</li>
-                <li className="c">Get a Quote</li>
+              <ul className="list-none  space-y-4">
+                <li className="hover:underline underline-offset-2">About us</li>
+                <li className="hover:underline underline-offset-2">Our Services</li>
+                <li className="hover:underline underline-offset-2">Testimonials</li>
+                <li className="hover:underline underline-offset-2">Get a Quote</li>
               </ul>
             </div>
             <div className="c">
               <ul className="list-none space-y-4">
-                <li className="c">FAQS</li>
-                <li className="c">Contact Us</li>
-                <li className="c">Safety Standards</li>
+                <li className="hover:underline underline-offset-2">FAQS</li>
+                <li className="hover:underline underline-offset-2">Contact Us</li>
+                <li className="hover:underline underline-offset-2">Safety Standards</li>
               </ul>
             </div>
           </div>
@@ -38,7 +38,7 @@ export default function Footer() {
             </p>
           </div>
           <div className="flex items-center gap-4">
-            <p className="underline">Privacy Policy</p>
+            <p className="underline underline-offset-2">Privacy Policy</p>
           </div>
         </div>
       </footer>
--- src/components/homePage/QuoteSection.tsx
+++ src/components/homePage/QuoteSection.tsx
@@ -4,15 +4,15 @@ import { Button } from "../ui/button";
 export default function QuoteSection() {
   return (
     <>
-        <section className="h-full bg-green-100 py-40">
-            <div className="w-10/12 mx-auto bg-green-500 rounded-xl">
+        <section className="h-full bg-green-9500 py-40">
+            <div className="w-10/12 mx-auto bg-green-950 text-slate-100 rounded-xl">
                 <div className="flex justify-between items-center px-8 py-12">
                     <div className="c">
                         <p className="text-4xl font-bold">Get Your Free Quote Today</p>
-                        <p>Contact us for reliable transportation solutions tailored for you</p>
+                        <p className="text-lg pt-4">Contact us for reliable transportation solutions tailored for you</p>
                     </div>
                     <div className="">
-                        <Button className="w-48">Qoute</Button>
+                        <Button type="button" className="w-48 text-lg bg-green-600 hover:bg-white hover:text-black hover:scale-105">Qoute</Button>
                     </div>
                 </div>
             </div>
