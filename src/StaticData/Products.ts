0dffeaab98ce3c856c72840884bd78ac46f103e2
Michael Gitonga <gitoshmbae@gmail.com>
2025-04-15 12:18:47 +0300

    Updates the sites favicon, adds tailwindscss typograophy plugin, adds moredetails to the products entries in src/StaticData/Products.ts

--- src/StaticData/Products.ts
+++ src/StaticData/Products.ts
@@ -4,7 +4,14 @@ export const products = [
     title: "AutoGas Dispensers",
     imagSrc: "Autogas_dispensers.jpg_y5ymrq",
     price: 0,
-    details: "Maximize your vehicle's efficiency with Levels Services Logistics's cutting-edge LPG Autogas Dispensers.",
+    details:
+      "Maximize your vehicle's efficiency with Levels Services Logistics's cutting-edge LPG Autogas Dispensers.",
+    moreDetails: `<ul>
+      <li>Our Autogas Dispensers are engineered for seamless refuelling, ensuring quick and efficient service for your vehicles. Enjoy hassle-free operations with our user-friendly design.</li>
+      <li>Crafted from premium materials and rigorously tested, our dispensers offer unparalleled durability and reliability. Trust in our quality's quality for long-lasting performance.</li>
+      <li>Our dedicated team of professionals is here to assist you at every turn. Whether you need guidance on dispenser selection or optimizing your refuelling process, rely on our expertise to drive your success.</li>
+      
+    </ul>`,
   },
   {
     id: 2,
@@ -13,41 +20,82 @@ export const products = [
     price: 0,
     details:
       "Experience efficient and reliable conversion of LPG from liquid to gaseous form with our high-quality Levels Services Logistics. The process of converting liquid LPG (liquefied petroleum gas) to its gaseous form involves utilizing heat to increase the temperature of the liquid, causing it to vaporize.",
+    moreDetails: `<ul>
+      <li>Engineered to seamlessly convert liquefied petroleum gas into vapor form, these vaporizers utilize a cutting-edge electric water bath system for consistent performance and precise control.</li>
+      <li>Choose from our full range of LPG vaporizers, meticulously designed to meet the demands of civil, industrial, and commercial applications.</li>
+      <li>Our vaporizers feature advanced heat exchanger technology, ensuring rapid and efficient conversion of LPG from liquid to gaseous form, ready for use.</li>
+      <li>Tailor your LPG supply to your specific needs with flow rates ranging from 50 to 400 Kg/hr, providing flexibility for diverse applications and usage levels.</li>
+      <li>Count on our vaporizers for consistent and reliable performance, delivering a steady supply of LPG gas for your operational needs.</li>
+    </ul>`,
   },
   {
     id: 3,
     title: "Forklift Cylinders",
     imagSrc: "Forklift-Cylinders_tmnjer",
     price: 0,
-    details: "Power your industrial operations with Levels Services Logistics's reliable Forklift Cylinders. We offer propane and butane forklift cylinders equipped with fill valves and gauges. Ideal for commercial use in large firms such as airports and industrial plants, these cylinders are designed for robust performance in demanding environments. Please note, forklift cylinders are not for domestic use and are exclusively available for large firms.",
+    details:
+      "Power your industrial operations with Levels Services Logistics's reliable Forklift Cylinders. We offer propane and butane forklift cylinders equipped with fill valves and gauges. Ideal for commercial use in large firms such as airports and industrial plants, these cylinders are designed for robust performance in demanding environments. Please note, forklift cylinders are not for domestic use and are exclusively available for large firms.",
+    moreDetails: `<ul>
+      <li>Our Forklift Cylinders are engineered for durability and reliability, ensuring uninterrupted operation in industrial settings. They meet stringent quality standards for dependable performance.</li>
+      <li>Tailored for large firms, our cylinders are ideal for heavy-duty applications in airports, warehouses, and industrial plants. Trust in Megtraco for cylinders that deliver consistent performance.</li>
+      <li>Made with precision and attention to detail, our cylinders come with fill valves and gauges for easy monitoring and refilling. Simplify your operations with our user-friendly design.</li>
+      <li>Please note that Forklift Cylinders are not intended for domestic use. They are exclusively available for large firms seeking reliable propane solutions for their industrial equipment.</li>
+    </ul>`,
   },
   {
     id: 4,
     title: "LPG Meters",
     imagSrc: "LPG-Meters_yhclwt",
     price: 0,
-    details: "Ensure accurate measurement and efficient management of your LPG consumption with our range of LPG meters at Levels Services Logistics.",
+    details:
+      "Ensure accurate measurement and efficient management of your LPG consumption with our range of LPG meters at Levels Services Logistics.",
+    moreDetails: `<ul>
+      <li>Choose from a variety of LPG meters to suit your specific needs, including liquid and vapor options.</li>
+      <li>Simplify billing and tracking with our pre-paid LPG meters, offering hassle-free management of gas consumption.</li>
+      <li>Select from traditional analogue meters or opt for modern IC card LPG meters, providing advanced functionality and data tracking capabilities.</li>
+      <li>Trust in the reliability of our mechanical LPG meters, offering accurate readings and long-lasting performance.</li>
+    </ul>`,
   },
   {
     id: 5,
     title: "Control Panel",
     imagSrc: "Control-Panel_k9y6k9",
     price: 0,
-    details: "",
+    details: "Revolutionize your LPG management with Level Services Logistics cutting-edge LPG Control Panel.",
+    moreDetails: `<ul>
+      <li>Our Control Panel is designed for effortless management of your LPG systems, ensuring optimal performance and safety. Streamline your operations with intuitive controls.</li>
+      <li>Crafted from premium materials and subjected to rigorous testing, our panels offer unmatched durability and reliability. Count on Megtraco's quality for long-lasting performance.</li>
+      <li>Embrace the latest innovations in LPG management with our Control Panel. Equipped with state-of-the-art features such as intuitive interfaces and precise monitoring systems, our panels redefine convenience.</li>
+      <li>Our experienced team is here to support you every step of the way. Whether you need guidance on panel selection or optimizing your LPG systems, trust in our expertise to drive your success.</li>
+    </ul>`,
   },
   {
     id: 6,
     title: "LPG Ball Valve",
     imagSrc: "lpg-ball-valve_g11npw",
     price: 0,
-    details: "Ensure safety, reliability, and precise control in your gas distribution lines with our premium LPG ball valves at Levels Services Logistics.",
+    details:
+      "Ensure safety, reliability, and precise control in your gas distribution lines with our premium LPG ball valves at Levels Services Logistics.",
+    moreDetails: `<ul>
+      <li>Our range of ball valves is suitable for a variety of industries and applications, including gas and LPG distribution lines, chemical processing, food plants, as well as air and water systems.</li>
+      <li>With reduced face-to-face dimensions, our ball valves offer efficient space utilization without compromising on performance.</li>
+      <li>Experience optimal tightness and minimal pressure drop with our ball valves, thanks to their intrinsic features designed for superior performance.</li>
+      <li>Crafted from high-quality materials, our ball valves are built to withstand the rigors of demanding environments, ensuring longevity and reliability.</li>
+    </ul>`,
   },
   {
     id: 7,
     title: "LPG Tank Accesories",
     imagSrc: "LPG-Tank-Accessories_ipj0va",
     price: 0,
-    details: "Enhance the performance and safety of your LPG tanks with our comprehensive range of tank accessories at Levels Services Logistics.",
+    details:
+      "Enhance the performance and safety of your LPG tanks with our comprehensive range of tank accessories at Levels Services Logistics.",
+    moreDetails: `<ul>
+      <li>Our multi-service valves provide versatile functionality, allowing for easy connection and control of LPG flow in both commercial and domestic settings.</li>
+      <li>Choose from our selection of high-pressure and low-pressure regulators, designed to ensure precise control and optimal performance of your LPG system.</li>
+      <li>Safeguard your tanks against overpressure situations with our pressure relief valves, offering reliable protection and peace of mind.</li>
+      <li>Streamline the refilling process with our filler valves, designed for efficient and convenient tank refills in commercial and domestic applications.</li>
+    </ul>`,
   },
   {
     id: 8,
@@ -55,19 +103,40 @@ export const products = [
     imagSrc: "Polyethylene-LPG-Piping_d6opv8",
     price: 0,
     details: "",
+    moreDetails: `<ul>
+      <li>Our polyethylene LPG piping is designed to withstand the demands of industrial and commercial applications, ensuring long-lasting performance and reliability.</li>
+      <li>Whether you need piping for a small-scale commercial operation or a large industrial facility, our polyethylene LPG piping offers versatile solutions to meet your specific needs.</li>
+      <li>Benefit from the inherent corrosion resistance of polyethylene, reducing the risk of leaks and maintenance issues over time.</li>
+      
+    </ul>`,
   },
   {
     id: 9,
     title: "LPG Gas Tank Equipment",
     imagSrc: "LPGAS-Train-Equipment_h19oau",
     price: 0,
-    details: "Ensure seamless and safe operation of your LPG systems with our comprehensive LP GAS Train Equipment at Levels Services Logistics.",
+    details:
+      "Ensure seamless and safe operation of your LPG systems with our comprehensive LP GAS Train Equipment at Levels Services Logistics.",
+    moreDetails: `<ul>
+      <li>Our LP GAS Train Equipment is designed to provide a comprehensive solution for gas trains, equipped with all the necessary accessories for efficient operation.</li>
+      <li>Prioritize safety in your LPG systems with our reliable equipment, engineered to meet rigorous industry standards and regulations.</li>
+      <li>Designed for hassle-free installation, our LP GAS Train Equipment streamlines the setup process, saving you time and effort.</li>
+      <li>Built to withstand the demands of industrial environments, our equipment boasts durable construction for long-lasting performance.</li>
+    </ul>`,
   },
   {
     id: 10,
     title: "LPG Tank",
     imagSrc: "LPG-Tank_szo9gf",
     price: 0,
-    details: "Looking for reliable LPG storage solutions? Look no further than Levels Services Logistics. We specialize in providing high-quality underground and above-ground commercial LPG tanks.",
+    details:
+      "Looking for reliable LPG storage solutions? Look no further than Levels Services Logistics. We specialize in providing high-quality underground and above-ground commercial LPG tanks.",
+    moreDetails: `<ul>
+      <li>Our LPG tanks come in sizes ranging from 250 kgs to 5 tonnes, catering to diverse commercial needs.</li>
+      <li>Choose from vertical or horizontal tanks, depending on your space and requirements</li>
+      <li>Ideal for properties with limited space or for those seeking a discreet installation. Our underground tanks are completely buried, with only the access cover visible, ensuring both safety and aesthetics.</li>
+      <li>For a more traditional setup, opt for our above-ground LPG storage tanks. These are installed on either concrete or prefabricated bases, providing stability and ease of access.</li>
+      <li>LPG tanks require sufficient space to maintain a safe distance of approximately 6 meters from surrounding buildings, ensuring compliance with safety regulations.</li>
+    </ul>`,
   },
 ];
--- src/app/products/[product]/page.tsx
+++ src/app/products/[product]/page.tsx
@@ -1,5 +1,6 @@
 import ImageView from "@/components/next-cloudinary/ImageView";
 import { products } from "@/StaticData/Products";
+import Link from "next/link";
 
 export default async function page({
   params,
@@ -8,13 +9,15 @@ export default async function page({
 }) {
   const { product } = await params;
   const Item = products.find((item) => item.id == parseInt(product));
+  const relatedProducts=products.slice(0,5)
+  
   return (
     <section className="min-h-screen">
       <div className="container mx-auto px-4 py-20">
         {Item != undefined ? (
           <div className="flex justify-evenly items-center gap-8">
             <div className="w-4/12">
-              {/* <div className="w-full h-96 bg-slate-100 rounded-lg"></div> */}
+              
               <ImageView
                 imageSrc={Item.imagSrc}
                 height={800}
@@ -24,12 +27,39 @@ export default async function page({
               />
             </div>
             <div className="w-5/12">
-              <p className="">
-                {Item.details}
-              </p>
+              <p className="leading-7 px-4">{Item.details}</p>
+              <div
+                className="py-8 px-4 prose"
+                dangerouslySetInnerHTML={{ __html: Item.moreDetails }}
+              ></div>
             </div>
           </div>
         ) : null}
+      <div className=" pt-20 pl-20">
+        <p className="text-xl font-bold pb-10">Related Products</p>
+        <div className="flex flex-wrap justify-evenly items-start gap-x-2 gap-y-20">
+          {relatedProducts.map((product) => (
+            // <Link href={`products/${product.id}`} key={product.id} className="w-2/12">
+            <Link href={`/products/${product.id}`} key={product.id} className="w-2/12">
+              <div >
+                <ImageView
+                  imageSrc={product.imagSrc}
+                  height={800}
+                  width={800}
+                  alt={product.title}
+                  classNames="object-contain h-64 w-full bg-slate-100 hover:bg-slate-200 rounded-lg p-1"
+                />
+                <div className="text- px-8 pt-4">
+                  {/* <p className="text-lg font-semibold">KSH</p> */}
+                  <p className="text- text-center font-semibold">
+                    {product.title}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
       </div>
     </section>
   );
